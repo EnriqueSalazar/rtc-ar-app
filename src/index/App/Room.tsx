752ecbc5375554a8da052b2449476738f037a1ff
Enrique Salazar <enrique.salazar@gmail.com>
2020-03-29 16:43:16 +0200

    Update Room.tsx

--- src/index/App/Room.tsx
+++ src/index/App/Room.tsx
@@ -49,6 +49,16 @@ export function Room() {
         ]
       },
       {
+        url: "turn:numb.viagenie.ca",
+        credential: "muazkh",
+        username: "webrtc@live.com"
+      },
+      {
+        urls: ["turn:13.250.13.83:3478?transport=udp"],
+        username: "YzYNCouZM1mhqhmseWk6",
+        credential: "YzYNCouZM1mhqhmseWk6"
+      },
+      {
         url: "turn:192.158.29.39:3478?transport=udp",
         credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
         username: "28224511:1379330808"
