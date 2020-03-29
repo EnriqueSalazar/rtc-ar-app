2c123c6e4de05dae91e83ba43f93f5f10cf0c35f
Enrique Salazar <enrique.salazar@gmail.com>
2020-03-29 15:43:42 +0200

    add google stuns

--- src/index/App/Room.tsx
+++ src/index/App/Room.tsx
@@ -40,7 +40,13 @@ export function Room() {
   const configuration = {
     iceServers: [
       {
-        urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302"
+        ]
       }
     ],
     iceCandidatePoolSize: 10
